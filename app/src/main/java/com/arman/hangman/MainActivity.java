5984d9beff334a6074c156272febf1fd2e9d269a
TikTaak <aramina1414@gmail.com>
2022-07-06 13:46:45 -0700

    gg

--- app/src/main/java/com/arman/hangman/MainActivity.java
+++ app/src/main/java/com/arman/hangman/MainActivity.java
@@ -3,6 +3,7 @@ package com.arman.hangman;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,63 +15,59 @@ import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imageview;
-    EditText edittext;
-    TextView textview;
-    Button button;
-    int status = 1;
-    String[] words = {"Ani", "Sam", "Joe"};
-    int rnd = new Random().nextInt(words.length);
-    String word = words[rnd];
+    private ImageView imageview;
+    private EditText edittext;
+    private TextView textview;
+    private TextView textview2;
+    private Button button;
+    private String word;
+    private int status = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        String[] arrayOfWords = {"hello", "mahin", "aghdas", "goodbye", "man", "woman"};
+        int rnd = new Random().nextInt(arrayOfWords.length);
+        word = arrayOfWords[rnd];
 
         imageview = findViewById(R.id.imageView);
         edittext = findViewById(R.id.editTextTextMultiLine);
         textview = findViewById(R.id.textView);
+        textview2 = findViewById(R.id.textView2);
         button = findViewById(R.id.button);
-
-        textview.setText(word.charAt(0));
-
+        textview.setText("");
+        textview2.setText(word);
+        for (int i = 0; i < word.length(); i++){
+            textview.setText(textview.getText().toString() + "- ");
+        }
 
     }
     public void buttonOnClick(View v){
-        List<Editable> playerGusses = new ArrayList<>();
-        playerGusses.add(edittext.getText());
-        for (int i = 1; i < word.length(); i++){
-            if (playerGusses.contains(word.charAt(i))) {
-                //textview.append(word.charAt(i));
-                //Add = word.charAt(i);
-                //textview.setText(textview.getText() + Add);
-                textview.setText(textview.getText().toString() + word.charAt(i));
-            }
-            else{
-                textview.setText(textview.getText().toString() + "-");
-            }
-        }
+        textview.setText("");
+        List<Character> playerGusses = new ArrayList<>();
+        String letterGuss = edittext.getText().toString();
+        playerGusses.add(letterGuss.charAt(0));
+        printWordState(word, playerGusses);
+
         ///////
 
     }
+    public void printWordState(String word, List<Character> playerGusses){
+        for (int i = 0; i < word.length(); i++){
+            if (playerGusses.contains(word.charAt(i))) {
 
+                textview.setText(textview.getText().toString() + word.charAt(i) + " ");
+            }
+            else{
+                textview.setText(textview.getText().toString() + "- ");
+            }
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-    public void buttonOnClick2 (View v){
+    public void incorrectSelection (View v){
         //textview.setText(word[1]);
         //imageview.setImageResource(R.drawable.hang1);
         if (status == 1){
@@ -114,7 +111,6 @@ public class MainActivity extends AppCompatActivity {
     public void hang6Img(){
         imageview.setImageResource(R.drawable.hang6);
     }
-
 
 
 }
